8421dbdb6ec8aeb230b82a6ef9f461e83dd75b49
mamoru12150927 <mamoru12150927@gmail.com>
2020-07-06 00:57:26 +0900

    first commit(嘘）

---
+++ skinservice/skinservice/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from skinservice.users.models import User
+from skinservice.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
